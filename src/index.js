6dafa8b8181ec3b3b7f2b6d8d95e6fe40bca14c2
Ihor Burenko <burenko@gmail.com>
2020-03-04 16:32:05 +0200

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -35,12 +35,40 @@ const MORSE_TABLE = {
     '---..':  '8',
     '----.':  '9',
     '-----':  '0',
+    ' ':      ' ',
 };
 
 function decode(expr) {
-    // write your solution here
+    let exprArr = [];
+    for (let i = 0; i < expr.length; i++) {
+        let charCount = Math.floor(i/10);
+        exprArr[charCount] ? exprArr[charCount]+=expr[i]: exprArr[charCount]=expr[i];
+    }
+    const newArr = exprArr.map(value => value.replace(/^0*/, ""));
+    return generatePairDigitArray(newArr).map(value => MORSE_TABLE[value]).join('');
 }
+
+function generatePairDigitArray( arr ) {
+    arr = arr.map(value => {
+        return (value === '**********') ? ' ' : value.split('');
+    });
+
+    let newArr = [];
+    for (let i = 0; i < arr.length; i++) {
+        for (let j = 0; j < arr[i].length; j++) {
+            let charCount = Math.floor(j/2);
+            if (!(newArr[i])) newArr.push([]);
+            newArr[i][charCount] ? newArr[i][charCount] += ( arr[i][j] ) : newArr[i][charCount] = ( arr[i][j] );
+            if (newArr[i][charCount].length === 2) {
+                newArr[i][charCount] = newArr[i][charCount]==='10' ? '.' : '-'
+            }
+        }
+        newArr[i] = newArr[i].join('');
+    }
+    return newArr
+}
+
 
 module.exports = {
     decode
-}
+};
